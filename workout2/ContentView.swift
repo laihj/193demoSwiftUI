55911e929c899132ba166f557fdd1682df24be94
laihj <hamainter@gmail.com>
2023-07-05 17:20:41 +0800

    Initial Commit

---
+++ workout2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  workout2
+//
+//  Created by laihj on 2023/7/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ workout2/workout2App.swift
@@ -0,0 +1,17 @@
+//
+//  workout2App.swift
+//  workout2
+//
+//  Created by laihj on 2023/7/5.
+//
+
+import SwiftUI
+
+@main
+struct workout2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
